0fe7617fd117af925efb1ac4e20cb2cf847c2c0c
LAPTOP-WALTHER-\Walther <waltherdegroot65@gmail.com>
2018-01-19 16:06:07 +0100

    Wekrend maar nog geen font

--- index.php
+++ index.php
@@ -1,26 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Good Day</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="./main.css">
+	</head>
+	<body>
+		<?php
 
-$date = date("h:i");
+		$date = date("H:i");
+		
+		if($date >"06:00" && $date < "12:00"){
+			echo '<h1 id="hey">Goede morgen!</h1>';
+			echo '<p id="tijd">Het is nu ' . $date . '</p>';
+			echo '<body style="background-image: url(./backgrounds/morning.png)">';
 
-if($date >"06:00" && $date < "12:00"){
-	echo 'Goede morgen!';
-	echo 'Het is nu ' . $date;
-	echo '<body style="background-image: url(./backgrounds/morning.png)">';
-
-}
-else if($date >"12:00" && $date < "18:00"){
-	echo 'Goede middag!';
-	echo 'Het is nu ' . $date;
-	echo '<body style="background-image: url(./backgrounds/afternoon.png)">';
-}
-else if($date >"18:00" && $date < "00:00"){
-	echo 'Goede avond!';
-	echo 'Het is nu ' . $date;
-	echo '<body style="background-image: url(./backgrounds/evening.png)">';
-}
-else{
-	echo 'Goede Nacht!';
-	echo 'Het is nu ' . $date;
-	echo '<body style="background-image: url(./backgrounds/evening.png)">';
-}
-?>
+		}
+		else if($date >"12:00" && $date < "18:00"){
+			echo '<h1 id="hey">Goede middag!</h1>';
+			echo '<p id="tijd">Het is nu ' . $date . '</p>';
+			echo '<body style="background-image: url(./backgrounds/afternoon.png)">';
+		}
+		else if($date >"18:00" && $date < "00:00"){
+			echo '<h1 id="hey">Goede avond!</h1>';
+			echo '<p id="tijd">Het is nu ' . $date . '</p>';
+			echo '<body style="background-image: url(./backgrounds/evening.png)">';
+		}
+		else{ 
+			echo '<h1 id="hey">Goede Nacht!</h1>';
+			echo '<p id="tijd">Het is nu ' . $date . '</p>';
+			echo '<body style="background-image: url(./backgrounds/evening.png)">';
+		}
+		?>
+	</body>
+</html>
