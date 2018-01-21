91833fe3b8efd5a06913bd43b03bea8ad78f5e2b
LAPTOP-WALTHER-\Walther <waltherdegroot65@gmail.com>
2018-01-21 17:52:23 +0100

    eerste commit

--- index.php
+++ index.php
@@ -9,27 +9,27 @@
 		<?php
 
 		$date = date("H:i");
-		
-		if($date >"06:00" && $date < "12:00"){
+
+		if($date > "06:00" && $date < "12:00"){
 			echo '<h1 id="hey">Goede morgen!</h1>';
 			echo '<p id="tijd">Het is nu ' . $date . '</p>';
 			echo '<body style="background-image: url(./backgrounds/morning.png)">';
 
 		}
-		else if($date >"12:00" && $date < "18:00"){
+		else if($date > "12:00" && $date < "18:00"){
 			echo '<h1 id="hey">Goede middag!</h1>';
 			echo '<p id="tijd">Het is nu ' . $date . '</p>';
 			echo '<body style="background-image: url(./backgrounds/afternoon.png)">';
 		}
-		else if($date >"18:00" && $date < "00:00"){
+		else if($date > "18:00" && $date < "23:59"){
 			echo '<h1 id="hey">Goede avond!</h1>';
 			echo '<p id="tijd">Het is nu ' . $date . '</p>';
 			echo '<body style="background-image: url(./backgrounds/evening.png)">';
 		}
-		else{ 
+		else if($date > "23:59" && $date < "06:00"){ 
 			echo '<h1 id="hey">Goede Nacht!</h1>';
 			echo '<p id="tijd">Het is nu ' . $date . '</p>';
-			echo '<body style="background-image: url(./backgrounds/evening.png)">';
+			echo '<body style="background-image: url(./backgrounds/night.png)">';
 		}
 		?>
 	</body>
